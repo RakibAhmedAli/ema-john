86cea77219024d31d2122308f7bc2ce7af078be7
Rakib Hossan <rakibahmedgraphic@gmail.com>
2020-04-18 11:48:31 +0600

    Cart Calculation Done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,17 +2,22 @@ import React, { useState } from 'react';
 import fakeData from '../../fakeData';
 import './Shop.css'
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
+
 
 const Shop = () => {
 
     const first10 = fakeData.slice(0,10);
     const [products, setProducts] = useState(first10);
 
+    const [cart, setCart] = useState([])
+
     const handleAddProduct = (product) => {
         console.log('product added', product);
+        const newCart = [...cart, product];
+        setCart(newCart);
     }
     
-
     return (
         <div className="shop-container">
             <div className="product-container">
@@ -25,7 +30,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h3>This is cart</h3>
+                <Cart cart = {cart}></Cart>
             </div>
             
         </div>
